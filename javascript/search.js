9522f75eb8a96c4b7acf3c0891f2cf8f6732192e
dmitr <my_name_dimon@vk.com>
2026-01-18 00:24:23 +0500

    make the mobileMenu

--- javascript/search.js
+++ javascript/search.js
@@ -1,6 +1,7 @@
 const searchBtn = document.getElementById("search-btn");
 const searchModal = document.getElementById("search-modal");
 const closeSearch = document.querySelector(".close-search");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 searchBtn.addEventListener("click", () => {
   searchModal.style.display = "block";
@@ -11,6 +12,17 @@ closeSearch.addEventListener("click", () => {
   searchModal.style.display = "none";
   document.body.style.overflow = "auto";
 });
+
+mobileMenu.addEventListener("click", () => {
+  const mainNav = document.querySelector(".main-nav")
+  if (mainNav.style.display === "none") {
+    mainNav.style.display = "block";
+  } else {
+    mainNav.style.display = "none";
+  }
+});
+
+
 
 window.addEventListener("click", (event) => {
   if (event.target == searchModal) {
