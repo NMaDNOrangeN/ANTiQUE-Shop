a60569fa4a70c23b5b852014046fb65510246773
nmadnorangen <caesgou@mail.ru>
2026-01-18 00:34:07 +0500

    добавил фавикон

--- javascript/search.js
+++ javascript/search.js
@@ -15,7 +15,7 @@ closeSearch.addEventListener("click", () => {
 
 mobileMenu.addEventListener("click", () => {
   const mainNav = document.querySelector(".main-nav")
-  if (mainNav.style.display === "none") {
+  if (mainNav.style.display === "none" || mainNav.style.display === "") {
     mainNav.style.display = "block";
   } else {
     mainNav.style.display = "none";
